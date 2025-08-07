1e354d41936cbbbd0746997d7627aecd8b8affd7
techiglu <techiglu@gmail.com>
2025-08-07 11:54:19 +0530

    Added prerender-routes.js

---
+++ src/prerender-routes.js
@@ -0,0 +1,12 @@
+export const prerenderRoutes = [
+  "/",
+  "/categories",
+  "/ai-agent",
+  "/about",
+  "/contact",
+  "/terms",
+  "/privacy",
+  "/advertise",
+  "/affiliate",
+  "/sitemap"
+];
